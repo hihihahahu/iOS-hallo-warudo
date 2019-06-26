78ed7ea2c204efb85aa5138cb321f4f696342929
Jeff Yang <www.speedway@hotmail.com>
2019-06-26 18:39:42 +0800

    first commit

---
+++ Hello world/Hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by borute on 6/26/19.
+//  Copyright © 2019 borute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
